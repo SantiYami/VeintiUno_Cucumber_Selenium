21b373a0c73fabaabd7ed0034157bba187e44b22
SantiYami <davidgarces.98@gmail.com>
2019-10-21 13:36:47 -0500

    Add files via upload

---
+++ Veintiuno/src/Juego/Carta.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Juego;
+
+/**
+ *
+ * @author david
+ */
+public class Carta {
+    private char simbolo; //2-3-4-5-6-7-8-9-D-J-Q-K-A D=diez
+    private char tipo;//P/C/R/T Picas/Corazones/Rombos/Treboles
+
+    public void setSimbolo(char simbolo) {
+        this.simbolo = simbolo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+
+    public char getSimbolo() {
+        return simbolo;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+    
+    public int obtenerValorSimbolo(){
+        if (this.simbolo=='D' || this.simbolo=='J' || this.simbolo=='Q' || this.simbolo=='K') {
+            return 10;
+        }else{
+            if (this.simbolo=='A'){
+                return 1;
+            }else{
+                return Integer.parseInt(this.simbolo+"");
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Carta{" + "simbolo=" + simbolo + ", tipo=" + tipo + '}';
+    }
+    
+    
+}
---
+++ Veintiuno/src/Juego/Mano.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Juego;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author david
+ */
+public class Mano {
+    ArrayList<Carta> arreglo = new ArrayList<>();
+    
+    public void añadirCarta(Carta carta){
+        arreglo.add(carta);
+    }
+    
+    public String toString(){
+        String mano = "";
+        for (int i = 0; i < arreglo.size(); i++) {
+            mano += arreglo.get(i).toString() + "";   
+        }
+        return mano;
+    }
+    
+    //Aqui seria donde se suma
+    public int obtenerValor(){
+        
+        int valor = 0;  //Valor de la mano
+        boolean as = false; //true si la mano tiene un as
+        
+        for (int i = 0; i < arreglo.size(); i++) {
+            Carta temp = arreglo.get(i);
+            int valorCarta = temp.obtenerValorSimbolo();
+            if (valorCarta == 1){   //Hay un as
+                as = true;
+            }
+            valor += valorCarta;
+        }
+        //Comprobacion del as
+        if (as == true && valor + 10 <= 21) {
+            valor = valor + 10;
+        }        
+        return valor;
+    }
+    
+}
